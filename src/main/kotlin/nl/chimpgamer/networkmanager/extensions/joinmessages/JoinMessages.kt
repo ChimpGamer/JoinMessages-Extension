b221e7a7869de0807817cf38c8682afacebf08ab
ChimpGamer <Tim.Cremers@Ziggo.nl>
2021-09-07 23:48:06 +0200

    Initial commit

---
+++ src/main/kotlin/nl/chimpgamer/networkmanager/extensions/joinmessages/JoinMessages.kt
@@ -0,0 +1,18 @@
+package nl.chimpgamer.networkmanager.extensions.joinmessages
+
+import nl.chimpgamer.networkmanager.api.extensions.NMExtension
+
+class JoinMessages : NMExtension() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+
+    override fun onConfigsReload() {
+
+    }
+}
